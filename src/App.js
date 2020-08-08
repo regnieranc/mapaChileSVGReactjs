a1fb0a2ce3902e1b658b4d9f73c8a760f1654ac2
Regnier <regnier.neira@virginiogomez.cl>
2020-08-08 18:16:28 -0400

    mapa chile interactivo reactjs

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import MapaChile from './components/MapaChile'
+
+export default class App extends Component{
+   constructor(props){
+      super(props)
+   }
+
+   render(){
+      return <MapaChile />
+   }
+}
